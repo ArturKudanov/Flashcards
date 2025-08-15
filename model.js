27785f08f5a9bc3cc8851704d913e8e8320d65d1
ArturKudanov <artur.kudanov.dev@gmail.com>
2025-08-15 11:10:30 -0700

    View task2

--- model.js
+++ model.js
@@ -1,8 +1,9 @@
 const fsp = require('fs').promises;
 const path = require('path');
+const { EOL } = require('os');
 
 class Model {
-  static async getTopics() {
+  static async getThemes() {
     const files = await fsp.readdir('./topics');
     return files.map((file) => ({
       name: path.basename(file, '_flashcard_data.txt'),
@@ -10,11 +11,11 @@ class Model {
     }));
   }
 
-  static async loadQestions(themFile) {
+  static async loadQuestions(themFile) {
     const content = await fsp.readFile(`./topics/${themFile}`, 'utf-8');
     const subArr = content
       .match(/([^\r\n]+)[\r\n]+([^\r\n]+)/gm)
-      .map((el) => el.trim().split('\r\n'));
+      .map((el) => el.trim().split(EOL));
     const arrContent = subArr.map((el) => ({ question: el[0], correct: el[1] }));
     return arrContent;
   }
